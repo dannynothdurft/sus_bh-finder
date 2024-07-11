0ad6bab1904ae3401b59f204695cba9fb564e9e7
danny <danny.nothdurft@sugarshape.de>
2024-07-11 08:31:37 +0200

    ss

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,7 +6,7 @@ function ProductList({ products }) {
     <AnimateHeight duration={200} height={"auto"}>
       <ul className="boxwrapper bh-finder__productListBoxWrapper clearfix">
         <div className="list-container">
-          <div className="gridView">
+          <div className="my-10 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mb-8">
             {products.map((item, index) => (
               <Tile key={item.node.id} product={item} position={index + 1} />
             ))}
